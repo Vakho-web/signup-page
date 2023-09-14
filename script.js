9a598e1350d447ee5109d4bd496c15d5ee2c7225
Vakho-web <vakhogvishiani@gmail.com>
2023-09-14 16:25:13 +0400

    IMPROVE password valiadtion

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById("check-btn");
+    
     const pass1 = document.getElementById("init-password");
     const pass2 = document.getElementById("conf-password");
     const output = document.getElementById("passcheck");
 
+    pass1.addEventListener("keyup", checkPassword)
     pass2.addEventListener("keyup", checkPassword)
-        function checkPassword() {
+        
+    function checkPassword() {
         if(pass1.value != pass2.value) {
             output.textContent = "Fix passwords"
         } else {
